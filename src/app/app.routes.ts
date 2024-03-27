7c834061e2cdbff0a69245ffb36770f20ed20183
Trần Vũ Quyết <quyettranvu@gmail.com>
2024-03-27 21:06:20 +0700

    Try using PrimeNg UI component, pre-template for signin and signup components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { WeatherComponent } from './weather/weather.component';
 import { EarthquakeComponent } from './earthquake/earthquake.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 //All routes defined from here
 export const routes: Routes = [
@@ -9,6 +11,14 @@ export const routes: Routes = [
   {
     path: 'earthquake',
     component: EarthquakeComponent,
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent,
+  },
+  {
+    path: 'register-user',
+    component: SignUpComponent,
   },
   {
     path: 'weather',
--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3001',
+  firebaseConfig: {
+    apiKey: "AIzaSyAvLBYG0rSDlaJywECrt7cmK7QnDZfH9eY",
+    authDomain: "angular-projects-778a0.firebaseapp.com",
+    projectId: "angular-projects-778a0",
+    storageBucket: "angular-projects-778a0.appspot.com",
+    messagingSenderId: "888533964102",
+    appId: "1:888533964102:web:eb26feb02f2767b1350147",
+    measurementId: "G-6M2MEK191S"
+  }
 };
